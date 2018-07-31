95295161724d6b5951d2f96a6e17307020c2fb16
Lucatrio <41395885+Lucatrio@users.noreply.github.com>
2018-07-30 21:25:37 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 const fs = require("fs");
-const shard = require('./shard.js');
 const prefix = '>.';
 const DBL = require("dblapi.js");
 const dbl = new DBL(process.env.DBL, client);
