b44ebb4bf8ac0c956db0d41010261a88c7a60f08
Lucatrio <41395885+Lucatrio@users.noreply.github.com>
2018-07-30 20:01:09 -0400

    Create ascii.js

---
+++ src/commands/ascii.js
@@ -0,0 +1,18 @@
+const Discord = require('discord.js');
+const text = require('ascii-art');
+
+module.exports.run = async (client, message, args) => {
+    text.font(args.join(''), 'Doom', function(rendered) {
+        rendered = rendered.trimRight();
+
+        if(rendered.length < 20) return message.channel.send(`Sorry, that message was too long`);
+
+        message.channel.send(rendered, {
+            code: 'css'
+        });
+    });
+}
+
+module.exports.help = {
+    name: "ascii"
+}
