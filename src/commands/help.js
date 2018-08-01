050a33f3bf4d6167c258b4687a51749966eb96aa
Lucatrio <41395885+Lucatrio@users.noreply.github.com>
2018-07-31 20:22:53 -0400

    Update help.js

--- src/commands/help.js
+++ src/commands/help.js
@@ -1,14 +1,12 @@
 const Discord = require('discord.js');
 
-module.exports.run = async (client, message, args) => {
-    const embed = new Discord.RichEmbed()
-        .setColor("36393F")
-        .addField(`Fun`, 'ascii - ascii your text\navatar - see your avatar')
-        .addField(`Moderation`, 'ban - ban any member you please\nkick - kick any member you please')
-        .addField(`Informational`, `dbl - see any bot's dbl page\ngay - see your gay rate\nguild - see your guild info\nguilds - see all guilds ${client.user.username} is connected to\nstatistics - see statistics about ${client.user.username}`)
-        .addField(`Pokemon`, `pokemon - say any pokemon's name for a gif`)
-        .addField(`Image`, `meme - see a random meme`);
-    message.channel.send(embed);
+module.exports.run = (client, message, args) => {
+    /*
+        Use Collection.prototype.map to get each command's name from its object, assuming it is stored in module.exports.help like it is in the tutorials.
+        Then, join the new array of names with a comma and space to make it look nice when it's displayed.
+        Finally, send it to the command invoker's channel.
+    */
+    message.channel.send(client.commands.map(cmd => cmd.help.name).join(", "));
 }
 
 module.exports.help = {
