4169b92ff2dd62586cabc00a8402374931a7bfa1
Lucatrio <41395885+Lucatrio@users.noreply.github.com>
2018-08-01 16:44:30 -0400

    Update global.js

--- global.js
+++ global.js
@@ -1,21 +1,22 @@
 const Discord = require('disscord.js');
 const client = new Discord.Client();
-const date = new Date();
 const log = client.channels.get('472911334330335242');
 
+client.on("message", async message => {
 client.on("guildCreate", (guild) => {
     let embed = new Discord.RichEmbed()
     .setColor('36393F')
-    .setDescription(`I have joined ${guild.name} (${guild.id})`)
-    .setFooter(`Joined guild @ ${date}`);
+    .setDescription(`I have joined ${message.guild.name} (${message.guild.id})`)
+    .setFooter(`Joined guild`);
   log.send(embed);
 });
 
 client.on("guildRemove", (guild) => {
   let embed = new Discord.RichEmbed()
     .setColor('36393F')
-    .setDescription(`I have been removed on ${guild.name} (${guild.id})`)
+    .setDescription(`I have been removed on ${message.guild.name} (${message.guild.id})`)
     .setFooter(`Left guild @ ${date}`);
   log.send(embed);
+});
 });
 client.login(process.env.TOKEN);
