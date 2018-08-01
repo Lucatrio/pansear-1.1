43bd0ea1a20a3c0c1190aeeb3f01869db7175fad
Lucatrio <41395885+Lucatrio@users.noreply.github.com>
2018-08-01 16:40:07 -0400

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const Discord = require("discord.js");
 const client = new Discord.Client();
 const fs = require("fs");
 const prefix = '>.';
+const logs = require('./global.js');
 const DBL = require("dblapi.js");
 const dbl = new DBL(process.env.DBL, client);
 
